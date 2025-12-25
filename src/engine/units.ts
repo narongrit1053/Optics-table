48c8f1b15c3372aee0a316bce58896426721de29
narongrit1053 <76087986+narongrit1053@users.noreply.github.com>
2025-12-25 15:10:30 +0700

    modified units

--- src/engine/units.ts
+++ src/engine/units.ts
@@ -8,18 +8,18 @@ export const DEFAULT_DIMENSIONS_MM = {
     mirror: { length: 10, width: 52.8, height: 52.8 }, // Thin mount, 25mm dia
     lens: { length: 25.4, width: 40, height: 50.2 },
     beamsplitter: { length: 12.7, width: 12.7, height: 12.7 }, // 1/2-inch cube approx
-    aom: { length: 30, width: 15, height: 15 },
+    aom: { length: 22.34, width: 50.76, height: 16 },
     detector: { length: 22.5, width: 50, height: 71 },
     fiber: { length: 20, width: 10, height: 10 },
     iris: { length: 5, width: 30, height: 30 },
     blocker: { length: 5, width: 30, height: 30 },
     cavity: { length: 100, width: 25, height: 25 }, // Variable length really
     text: { length: 50, width: 20, height: 0 },
-    hwp: { length: 15.5, width: 55.4, height: 55.4 },
-    qwp: { length: 15.5, width: 55.4, height: 55.4 },
-    polarizer: { length: 15.5, width: 55.4, height: 55.4 },
+    hwp: { length: 15.5, width: 50, height: 50 },
+    qwp: { length: 15.5, width: 50, height: 50 },
+    polarizer: { length: 15.5, width: 50, height: 50 },
     pbs: { length: 12.7, width: 12.7, height: 12.7 },
     poldetector: { length: 12, width: 12, height: 12 },
-    vaporcell: { length: 75, width: 25, height: 25 },
+    vaporcell: { length: 108, width: 25, height: 25 },
     breadboard: { length: 300, width: 300, height: 10 }
 };
